a02d1a52831d12d9c1715a74c134ed6b9d882bab
Aminu <shehuumara@gmail.com>
2021-09-27 02:07:07 +0300

    Comments added

--- emailGenerator/emailGeneratorProgram/main.cpp
+++ emailGenerator/emailGeneratorProgram/main.cpp
@@ -16,37 +16,42 @@ int main(int argc, char* argv[])
     QStringList generated;
     QStringList input;
 
+    // create a file for error messages
     char path[14] = "errorFile.txt";
     QString errorFilePath = getFileLocation(path);
     QStringList string;
     QFile errorFile("errorFile.txt");
 
-    if (argc < 3)
+    if (argc < 3) // when one or both files are not provided
     {
         string.append("Insufficient arguments. Please provide all the files.");
 
-        writeOutput(errorFilePath, string);
-        errorFile.close();
-        return 0;
+        writeOutput(errorFilePath, string); // write error message to the error file
+        errorFile.close(); //close error file
+        return 0; // end the program execution
     }
-    if (argc > 3)
+    if (argc > 3) // when two many aruments were provided
     {
         string.append("Too many files provided");
 
-        writeOutput(errorFilePath, string);
-        errorFile.close();
-        return 0;
+        writeOutput(errorFilePath, string); // write error message to the error file
+        errorFile.close(); //close error file
+        return 0; // end the program execution
     }
 
-    QString templateFilePath = getFileLocation(argv[1]);
-    QString dataFilePath = getFileLocation(argv[2]);
+    QString templateFilePath = getFileLocation(argv[1]); // get the location of file with the email template
+    QString dataFilePath = getFileLocation(argv[2]); // get the location of file with the data
 
+    // try getting the composition of the files
     try {
-        temp = getFileData(templateFilePath);
-        data = getFileData(dataFilePath);
 
-        for(int i=0; i < data.size(); i++)
+        //-------------------------------If no error occured----------------------------------------
+        temp = getFileData(templateFilePath); // save the composition of template file
+        data = getFileData(dataFilePath); // save the composition of data file
+
+        for(int i=0; i < data.size(); i++) // for each of data in data file
         {
+            //-------create a file name "genarated(s)" where 's' is the number of generated files---------
             QString s = QString::number(i+1);
 
             QString out = "generated";
@@ -57,28 +62,35 @@ int main(int argc, char* argv[])
             QByteArray ba = out.toLocal8Bit();
             char *outp = ba.data();
 
+            //-------create an output file with the created file name---------
             QString outputFilePath = getFileLocation(outp);
             QFile outputFile(outputFilePath);
 
+            // take a current line of the data file
             QStringList singleData;
             singleData.append(data[i]);
 
+            // try generatingg email for the current line of the data file from the template given
             try {
+                // save the generated email if no error occured
                 generated = generateEmail(temp, singleData);
             } catch (int err) {
+
+                //------write an error message to thhe error file if an error occured--------
                 string.append("A value for key was found in the data list that is not included in the template.");
 
                 writeOutput(errorFilePath, string);
-                errorFile.close();
-                return 0;
+                errorFile.close(); // close the erro file
+                return 0; // end program execution
             }
 
-            writeOutput(outputFilePath, generated);
-            out.clear();
+            writeOutput(outputFilePath, generated); // write the generated email to the created output file
+            out.clear(); // clear the created file name to create a new one for the next email to be generated
 
         }
 
     } catch (int err) {
+    //------write an error message to thhe error file if an error occured an end the program execution--------
         switch (err)
         {
         case 1:
