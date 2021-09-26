7b3ff17608faf5a3e585383a40283d61112c5b03
Aminu <shehuumara@gmail.com>
2021-09-27 02:21:52 +0300

    Program refactored

--- emailGenerator/emailGeneratorProgram/functions.cpp
+++ emailGenerator/emailGeneratorProgram/functions.cpp
@@ -3,15 +3,15 @@
 
 void writeOutput(QString filePath, QStringList lines)
 {
-    QFile outputFile(filePath);
+    QFile outputFile(filePath); // create an output file
 
-    if (outputFile.open(QIODevice::WriteOnly | QIODevice::Text))
+    if (outputFile.open(QIODevice::WriteOnly | QIODevice::Text)) // open the created file
     {
         QTextStream stream(&outputFile);
 
-        for (int i = 0; i < lines.length(); i++)
-            stream << lines[i] << endl;
-        outputFile.close();
+        for (int i = 0; i < lines.length(); i++) // for each line of the texts
+            stream << lines[i] << endl; // write the line to the output file
+        outputFile.close(); // close the output file
     }
 }
 
@@ -60,29 +60,30 @@ QStringList getFileData(QString filePath) throw(int)
 
 QStringList generateEmail(QStringList temp, QStringList data) throw (int)
 {
-    QStringList generated = temp;
-    bool isFound = false;
-    QStringList dataList = data[0].split(" ");
+    QStringList generated = temp; // savee the given template
+    bool isFound = false; // set key found to false
 
-      for(int i = 0; i < dataList.size(); i++)
+    QStringList dataList = data[0].split(" "); // split the data line with space to seperate key and its value
+
+      for(int i = 0; i < dataList.size(); i++) // for each of the words of the splited data
        {
-
-         for(int j=0; j < generated.size(); j++)
+         for(int j=0; j < generated.size(); j++) // for each line of the given templated
          {
-           if (generated[j].contains(dataList[i]))
+           if (generated[j].contains(dataList[i])) // if the line of the template contains the current key of the data
            {
+           //replace the key in the template file with the next word of the data (the value of the key)
                generated[j].replace(dataList[i], dataList[i+1].replace(R"(_)", R"( )"));
-               isFound = true;
+               isFound = true; // set key found to true
            }
          }
 
-         i += 1;
-         if(isFound == false)
+         i += 1; // skip to the next key
+         if(isFound == false) // throw an error if the key from the data list is not found in the tempalte
          {
              throw 1;
          }
          isFound = false;
        }
 
-    return generated;
+    return generated; // return the generated email for the given data
 }
